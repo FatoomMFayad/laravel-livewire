60d0a824716a7132f0d213863aa7958768916a7f
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-11 13:12:21 +0200

    add modal form to pages livewire

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
 use Livewire\Component;
 
 class Pages extends Component
 {
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+
+    /**
+     * createShowModal
+     * of the create function.
+     * @return void
+    **/
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+    /**
+     * The livewire render function
+     *
+     * @return void
+     **/
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,24 @@
-<div>
-    Pages livewire component
+<div class="p-6">
+    <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+        <x-jet-button wire:click="createShowModal">
+            {{__('Create')}}
+        </x-jet-button>
+    </div>
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{__('Save Page')}}
+        </x-slot>
+        <x-slot name="content">
+            The form elements go here
+        </x-slot>
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{__('Nevermind')}}
+            </x-jet-secondary-button>
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{__('Save')}}
+            </x-jet-button>
+
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
