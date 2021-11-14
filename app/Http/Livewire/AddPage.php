7045f1b78a5b9692620f4b19a5afed4cb747734a
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-14 09:22:01 +0200

    stash

---
+++ app/Http/Livewire/AddPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-page');
+    }
+}
