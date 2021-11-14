bbc888d3396b7cbb07afbf2fb5bc4666ef19d2da
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-14 14:37:51 +0200

    slugurl

--- app/Http/Livewire/Frontpage.php
+++ app/Http/Livewire/Frontpage.php
@@ -3,9 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Page;
 
 class Frontpage extends Component
 {
+    public $title;
+    public $content;
+
+    public function mount($urlslug)
+    {
+        $this->urlslug = $urlslug;
+    }
     public function render()
     {
         return view('livewire.frontpage')->layout('layouts.frontpage');
